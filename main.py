794811244c8ec6c4b5de180e4b9d7995a9d881c4
czc <147292653+czc6666@users.noreply.github.com>
2024-06-06 19:05:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import AndroZoo_Download_function
+import AndroZoo_Download_function as AD
+import os
+from datetime import datetime
 
 apikey = '输入你的api'
-debug = True
 configs = {
     'start_year': 2014,
     'end_year': 2014,
@@ -12,13 +13,12 @@ configs = {
 if __name__ == '__main__':
     output_dir = datetime.now().strftime("%Y%m%d")
     output_dir += '_' + '_'.join([str(configs[c]) for c in configs])
-    debug_print('生成下载目录名字')
+    AD.debug_print('生成下载目录名字')
     os.makedirs(output_dir, exist_ok=True)
-    debug_print('创建下载目录完成')
-    df = filter_apk(configs, output_dir, random_selection=True, random_sample_size=40000)
-    debug_print('apk过滤完成')
-    links_dir = generate_download_link(df, output_dir)
-    debug_print('下载链接txt生成完成')
-
-    debug_print('开始多线程下载')
-    download_apk_multithreaded(links_dir, output_dir, num_threads=200)
+    AD.debug_print('创建下载目录完成')
+    df = AD.filter_apk(configs, output_dir, random_selection=True, random_sample_size=40000)
+    AD.debug_print('apk过滤完成')
+    links_dir = AD.generate_download_link(df, output_dir)
+    AD.debug_print('下载链接txt生成完成')
+    AD.debug_print('开始多线程下载')
+    AD.download_apk_multithreaded(links_dir, output_dir, num_threads=200)
